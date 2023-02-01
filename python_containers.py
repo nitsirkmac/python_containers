7c2b40b4d0d7b2bd7c2b89dad91617277fccdf67
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-02-01 16:54:19 -0500

    exercise 7 complete

--- python_containers.py
+++ python_containers.py
@@ -1,14 +1,18 @@
 ## Exercise 1 ##
 
+
 students = [ "Molly", "Mickey", "Michael", "Matthew" ]
 print( students[1])
 print( students[-1])
 
+
 ## Exercise 2 ##
 
+"""
 foods = ("Bread", "Bacon", "Lettuce", "Tomato")
 for food in foods:
     print(f"{food} is a good food")
+"""
 
 ## Exercise 3 ##
 
@@ -16,7 +20,7 @@ for food in foods:
 
 
 ## Exercise 4 ##
-
+"""
 home_town = {
     "city": "Hatteras",
     "state": "North Carolina",
@@ -24,14 +28,28 @@ home_town = {
 }
 
 print( f"I was born in {home_town['city']}, {home_town['state']} - population of {home_town['population']}")
+"""
 
 ## Exercise 5 ##
-
+"""
 for key, val in home_town.items():
     print(f'"{key} = {val}"')
+"""
 
 ## Exercise 6 ##
 
 cohort = []
+cohort.extend(('student', students[0]))
+print(cohort)
+# dict(item([("student", "Molly"), ("fav_food": "Bread")]))
 
-# dict_items([("student", "Molly"), ("fav_food": "Bread")])
+
+## Exercise 7 ##
+"""
+awesome_students = []
+
+for student in students:
+        awesome_students.append(f'{student} is awesome!')
+
+print(awesome_students)
+"""
