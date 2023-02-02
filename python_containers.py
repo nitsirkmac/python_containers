406fcbb2f2911a9f5fb7d2202f78c363288785e2
Krissy Macdonald <krissymacdonald5@gmail.com>
2023-02-01 19:23:55 -0500

    homework complete

--- python_containers.py
+++ python_containers.py
@@ -1,17 +1,17 @@
 ## Exercise 1 ##
 
-
+"""
 students = [ "Molly", "Mickey", "Michael", "Matthew" ]
 print( students[1])
 print( students[-1])
-
+"""
 
 ## Exercise 2 ##
-
+"""
 foods = ("Bread", "Bacon", "Lettuce", "Tomato")
 for food in foods:
     print(f"{food} is a good food")
-
+"""
 ## Exercise 3 ##
 """
 for food in foods[2:]:
@@ -38,14 +38,13 @@ for key, val in home_town.items():
 """
 
 ## Exercise 6 ##
-
+"""
 cohort = []
 for idx, student in enumerate(students):
     cohort.append(dict(iter([("student", student), ("fav_food", foods[idx])])))
 
 print(cohort)
-
-
+"""
 
 ## Exercise 7 ##
 """
@@ -53,7 +52,6 @@ awesome_students = [f'{student} is awesome!' for student in students]
 
 print(awesome_students)
 """
-
 ## Exercise 8 ##
 """
 aFoods = [food for food in foods if 'a' in food]
